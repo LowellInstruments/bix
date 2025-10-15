280ca1820f0283ee36eaf0a5e1e7affb51dbe0e4
kaz <kaz>
2025-10-15 10:42:52 -0400

    more

--- scripts/run_bix.sh
+++ scripts/run_bix.sh
@@ -6,9 +6,18 @@ FOL=/home/kaz/PycharmProjects/bix
 
 clear
 echo
+
+
 echo "activating python venv for BIX"
 cd $FOL || (echo "BIX error, cannot switch folder"; exit 1)
 source .venv/bin/activate
+
+
+
+echo "getting last BIX code"
+git pull
+
+
 
 echo "running main_bix.py"
 python3 main_bix.py
